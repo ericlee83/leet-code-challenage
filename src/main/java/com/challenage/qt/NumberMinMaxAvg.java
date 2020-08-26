fc6c41e3c28049c4e1afc1956ef97007037c15c6
ericlee83 <eric-lee-83@hotmail.com>
2020-08-26 17:13:41 +1000

    use parallel

--- src/main/java/com/challenage/qt/NumberMinMaxAvg.java
+++ src/main/java/com/challenage/qt/NumberMinMaxAvg.java
@@ -2,8 +2,15 @@ package com.challenage.qt;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NumberMinMaxAvg {
     private static DecimalFormat df = new DecimalFormat("#.##");
@@ -12,8 +19,7 @@ public class NumberMinMaxAvg {
             Scanner sc = new Scanner(inputStream, "UTF-8");
             int lineNumber = 1;
             while (sc.hasNextLine()) {
-                String result = parseLine(lineNumber++,sc.nextLine());
-                System.out.println(result);
+                System.out.println(String.format("%d,%s",lineNumber++,parseLine(sc.nextLine())));
             }
             if (sc.ioException() != null) {
                 throw sc.ioException();
@@ -22,8 +28,25 @@ public class NumberMinMaxAvg {
             e.printStackTrace();
         }
     }
+    public void solutionParallel() throws IOException {
+        URL url = getClass().getClassLoader().getResource("input.txt");
+        final List<String> lines = Files.readAllLines(Paths.get(url.getPath()));
+        IntStream.range(0,lines.size())
+                .mapToObj(index->String.format("%d;%s",index+1,lines.get(index)))
+                .parallel()
+                .map(line->parseLineParallel(line))
+                .peek(System.out::println)
+                .collect(Collectors.toList());
 
-    public String parseLine(int lineNumber, String line){
+    }
+
+    public String parseLineParallel(String line){
+        String[] info = line.split(";");
+        return String.format("%s,%s",info[0],parseLine(info[1]));
+    }
+
+
+    public String parseLine(String line){
         double avg = 0.00;
         int v = 0;
         int count = 1;
@@ -35,6 +58,6 @@ public class NumberMinMaxAvg {
             max = v>max?v:max;
             avg = avg - (avg - v)/ count++;
         }
-        return String.format("%d,%d,%s,%d,%d",lineNumber,count-1,df.format(avg),max,min);
+        return String.format("%d,%s,%d,%d",count-1,df.format(avg),max,min);
     }
 }
