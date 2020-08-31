674072e927e2085eb9bcda90fd0f0a080a9edf4e
ericlee83 <eric-lee-83@hotmail.com>
2020-08-31 16:56:15 +1000

    a review of decorate pattern

--- src/main/java/com/pattern/structural/decorator/ChristmasTree.java
+++ src/main/java/com/pattern/structural/decorator/ChristmasTree.java
@@ -1,5 +1,10 @@
 package com.pattern.structural.decorator;
 
+/**
+ * When we wish to add, enhance or even remove the behavior or state of objects
+ * When we just want to modify the functionality of a single object of class and leave others unchanged
+ */
+
 public interface ChristmasTree {
     String decorate();
 }
